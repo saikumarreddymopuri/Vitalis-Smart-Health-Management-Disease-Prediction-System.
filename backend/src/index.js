bf09edb023d0d8aaa08658e98a32749de3c65f83
saikumarredyymopuri <saikumarmopuri@gmail.com>
2025-06-07 23:14:11 +0530

    db connection

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+//require('dotenv').config( {path: './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+//import { app } from "./app.js";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 4000, () => {
+//         console.log(`Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((error) => {
+//     console.log("Mongo db connection failed !!! ",error);
+    
+// })
+
