db50d932a9532280f16e5a04bf66f3c350e2ee81
saikumarredyymopuri <saikumarmopuri@gmail.com>
2025-06-07 23:43:18 +0530

    updation of utils - custom api errors and response and cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-//import { app } from "./app.js";
+import { app } from "./app.js";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 
@@ -12,13 +12,13 @@ dotenv.config({
 
 
 connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 4000, () => {
-//         console.log(`Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((error) => {
-//     console.log("Mongo db connection failed !!! ",error);
+.then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongo db connection failed !!! ",error);
     
-// })
+})
 
